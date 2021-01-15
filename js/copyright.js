6f99e8c03f14e3c0138355110510d6e9d6e85471
ruthtarts <ruthmmecham@byui.edu>
2021-01-14 19:02:46 -0700

    Initial commit

--- js/copyright.js
+++ js/copyright.js
@@ -1,3 +1,9 @@
-document.write(new Date().getFullYear( )
-
-)
+window.addEventListener('load', (
+    function () {
+        document.getElementById('copyright-year').appendChild(
+            document.createTextNode(
+                new Date().getFullYear()
+            )
+        );
+    }
+));
