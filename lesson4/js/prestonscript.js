2328b0579e017d1503a04ca1a61d84671f262cc7
ruthtarts <ruthmmecham@byui.edu>
2021-02-01 16:56:12 -0700

    Initial commit

--- lesson4/js/prestonscript.js
+++ lesson4/js/prestonscript.js
@@ -1,8 +1,7 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.menu')
+const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
+window.onresize = () => {if (window.innerWidth > 760)
+mainnav.classList.remove('responsive')};
