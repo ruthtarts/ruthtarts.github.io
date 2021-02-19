2eb9d2e1b4f1911988fd8be0cefb904a1ed4bc3f
ruthtarts <ruthmmecham@byui.edu>
2021-02-18 22:23:38 -0700

    Initial commit

---
+++ lesson7/lazyload.js
@@ -0,0 +1,37 @@
+let imagesToLoad = document.querySelectorAll('img[dta-src]');
+
+let imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 100px 0px"
+
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload= () => {
+        image.removeAttribute('data-src');
+
+    };
+};
+
+if ('IntersectionObserver' in window {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                return;
+            }
+            else {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions)
+
+    imagesToLoad.forEach((image) => {
+        imgObserver.observe(image);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
