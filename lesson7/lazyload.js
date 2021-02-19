ba0f9eaf45a8c63d9dc1dfebc68718d6861c7cfd
ruthtarts <ruthmmecham@byui.edu>
2021-02-18 22:36:56 -0700

    Initial commit

--- lesson7/lazyload.js
+++ lesson7/lazyload.js
@@ -4,15 +4,15 @@ let imgOptions = {
     threshold: 0,
     rootMargin: "0px 0px 100px 0px"
 
-};
+}
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload= () => {
         image.removeAttribute('data-src');
 
-    };
-};
+    }
+}
 
 if ('IntersectionObserver' in window {
     const imgObserver = new IntersectionObserver((items, observer) => {
@@ -24,14 +24,14 @@ if ('IntersectionObserver' in window {
                 loadImages(item.target);
                 imgObserver.unobserve(item.target);
             }
-        });
+        })
     }, imgOptions)
 
     imagesToLoad.forEach((image) => {
         imgObserver.observe(image);
-    });
+    })
 } else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
-    });
+    })
 }
