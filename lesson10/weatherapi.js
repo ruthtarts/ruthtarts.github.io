52405b7a5f7b109a508c76f4abce7aba4955b303
ruthtarts <ruthmmecham@byui.edu>
2021-03-09 12:59:35 -0700

    Initial commit

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -1,17 +1,21 @@
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=c6de52252b8d907f2b675bd79cb5cd2b&units=imperial";
 
 fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
 
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('high-temp').textContent = jsObject.main.temp_max;
+        document.getElementById('wind-speed').textContent = jsObject.wind.speed;
+        document.getElementById('wind-chill').textContent = jsObject.main.feels_like;
+        document.getElementById('humid').textContent = jsObject.main.humidity;
 
- const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'
-const desc = jsObject.weather[0].description;
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'
+        const desc = jsObject.weather[0].description;
 
-document.getElementById('imagesrc').textContent = imagesrc;
-document.getElementById('icon').setAttribute('src', imagesrc);
-document.getElementById('icon').setAttribute('alt', desc);
+        document.getElementById('imagesrc').textContent = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
 
-  });
+    });
