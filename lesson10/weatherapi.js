e4e40a15bf06b05e94722163de4f8fa89692c0d3
ruthtarts <ruthmmecham@byui.edu>
2021-03-16 19:39:50 -0600

    Initial commit

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -17,62 +17,28 @@ fetch(apiURL)
     fetch(apiURL2)
     .then((response) => response.json())
     .then((jsObject) => {
-      console.log(jsObject);
-
-        //Display icon
-
-        let img1 = 'https://openweathermap.org/img/w/' + jsObject.list[1].weather[0].icon + '.png';        
-        let desc1 = jsObject.list[1].weather[0].description;        
-        let img2 = 'https://openweathermap.org/img/w/' + jsObject.list[2].weather[0].icon + '.png';        
-        let desc2 = jsObject.list[2].weather[0].description; 
-        let img3 = 'https://openweathermap.org/img/w/' + jsObject.list[3].weather[0].icon + '.png';        
-        let desc3 = jsObject.list[3].weather[0].description; 
-        let img4 = 'https://openweathermap.org/img/w/' + jsObject.list[4].weather[0].icon + '.png';        
-        let desc4 = jsObject.list[4].weather[0].description; 
-        let img5 = 'https://openweathermap.org/img/w/' + jsObject.list[5].weather[0].icon + '.png';        
-        let desc5 = jsObject.list[5].weather[0].description; 
+        console.table(jsObject);
 
 
-        document.getElementById('icon1').setAttribute('src', img1);               
-        document.getElementById('icon1').setAttribute('alt', desc1);   
-        document.getElementById('icon2').setAttribute('src', img2);               
-        document.getElementById('icon2').setAttribute('alt', desc2); 
-        document.getElementById('icon3').setAttribute('src', img3);               
-        document.getElementById('icon3').setAttribute('alt', desc3); 
-        document.getElementById('icon4').setAttribute('src', img4);               
-        document.getElementById('icon4').setAttribute('alt', desc4); 
-        document.getElementById('icon5').setAttribute('src', img5);               
-        document.getElementById('icon5').setAttribute('alt', desc5); 
+        const list = jsObject['list'];
+        let zero = 0;
 
-      //Display days  
+        for (let i = 0; zero < 5; i++) { // temporary checking for valid response and data parsing
+            if (list[i].dt_txt.includes("18:00:00")) {
+                zero++;
 
-      const daylist = ["Sun", "Mon",  "Tue", "Wed", "Thu", "Fri", "Sat"];
-      let date1 = new Date(jsObject.list[1].dt_txt);
-      let date2 = new Date(jsObject.list[2].dt_txt);
-      let date3 = new Date(jsObject.list[3].dt_txt);
-      let date4 = new Date(jsObject.list[4].dt_txt);
-      let date5 = new Date(jsObject.list[5].dt_txt);
-      let day1 = date1.getDay();
-      let day2 = date2.getDay();
-      let day3 = date3.getDay();
-      let day4 = date4.getDay();
-      let day5 = date5.getDay();
-  
-          document.getElementById('day1').textContent = daylist[day1];
-          document.getElementById('day2').textContent = daylist[day2];
-          document.getElementById('day3').textContent = daylist[day3];
-          document.getElementById('day4').textContent = daylist[day4];
-          document.getElementById('day5').textContent = daylist[day5];
-
-        //Display temp
-
-        document.getElementById('temp1').textContent = Math.round(jsObject.list[1].main.temp);
-        document.getElementById('temp2').textContent = Math.round(jsObject.list[2].main.temp);
-        document.getElementById('temp3').textContent = Math.round(jsObject.list[3].main.temp);
-        document.getElementById('temp4').textContent = Math.round(jsObject.list[4].main.temp);
-        document.getElementById('temp5').textContent = Math.round(jsObject.list[5].main.temp);
-
-
-
-        });
-   
+                let daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+                let date = new Date(list[i].dt_txt);
+                let day = date.getDay();
+                let daysOfWeek = daylist[day];
+                let image = 'https://openweathermap.org/img/w/' + list[i].weather[0].icon + '.png';
+                let desc = list[i].weather[0].description; 
+            
+                document.getElementById(`day` + zero).textContent = daylist[day];
+                document.getElementById(`temp` + zero).textContent = Math.round(list[i].main.temp);
+                document.getElementById(`icon` + zero).setAttribute('src', image);
+                document.getElementById(`icon` + zero).setAttribute('alt', desc);
+              }
+            }
+    
+    });
