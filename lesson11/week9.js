7684df1e47dec84222993a90fd297451367333b9
ruthtarts <ruthmmecham@byui.edu>
2021-03-19 23:50:20 -0600

    Initial commit

--- lesson11/week9.js
+++ lesson11/week9.js
@@ -12,33 +12,34 @@ fetch(requestURL)
             if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs") {
 
 
-            let areas = document.createElement('section');
-            let townName = document.createElement('h2');
-            let photo = document.createElement('img');
-            let townMotto = document.createElement('h4');
-            let yearFounded = document.createElement('div');
-            let currentPopulation = document.createElement('div');
-            let averageRainfall = document.createElement('div');
+                let areas = document.createElement('section');
+                let townName = document.createElement('h2');
+                let photo = document.createElement('img');
+                let townMotto = document.createElement('h4');
+                let yearFounded = document.createElement('div');
+                let currentPopulation = document.createElement('div');
+                let averageRainfall = document.createElement('div');
 
-            townName.textContent = towns[i].name;
-            townMotto.textContent = towns[i].motto;
-            yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
-            averageRainfall.textContent = 'Average Rain Fall: ' + towns[i].averageRainfall;
-            
-            photo.setAttribute('src', towns[i].photo);
-            photo.setAttribute('alt', towns[i].name);
+                townName.textContent = towns[i].name;
+                townMotto.textContent = towns[i].motto;
+                yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
+                averageRainfall.textContent = 'Average Rain Fall: ' + towns[i].averageRainfall;
 
-            areas.appendChild(townName);
-            areas.appendChild(townMotto);
-            areas.appendChild(yearFounded);
-            areas.appendChild(currentPopulation);
-            areas.appendChild(averageRainfall);
-            areas.appendChild(photo);
+                photo.setAttribute('src', towns[i].photo);
+                photo.setAttribute('alt', towns[i].name);
 
-            document.querySelector('div.areas').appendChild(areas);
+                areas.appendChild(townName);
+                areas.appendChild(townMotto);
+                areas.appendChild(yearFounded);
+                areas.appendChild(currentPopulation);
+                areas.appendChild(averageRainfall);
+                areas.appendChild(photo);
+
+                document.querySelector('div.areas').appendChild(areas);
 
 
-                }        }
+            }
+        }
 
     });
