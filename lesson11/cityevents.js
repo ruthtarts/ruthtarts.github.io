01c892d7ccb4bd6ad1f6d9421839090498f11a94
ruthtarts <ruthmmecham@byui.edu>
2021-03-19 20:31:34 -0600

    Initial commit

---
+++ lesson11/cityevents.js
@@ -0,0 +1,37 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+
+        const towns = jsonObject["towns"];
+
+        for (let i = 0; i < towns.length; i++) { // temporary checking for valid response and data parsing
+            if (towns[i].name == "Preston") {
+
+                let place = document.createElement('section');
+                let prestonEvents = document.createElement('div');
+
+                for (let j = 0; j < towns[i].events.length; j++) {
+
+
+                    prestonEvents.append(towns[i].events[j]);
+                    if (j < towns[i].events.length - 1) {
+                        prestonEvents.append(" | ");
+                    }
+
+                }
+
+                place.appendChild(prestonEvents);
+
+
+                document.querySelector('div.prestonEvents').appendChild(place);
+
+
+            }
+        }
+
+    });
---
+++ lesson11/fhweatherapi.js
@@ -0,0 +1,49 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&appid=c6de52252b8d907f2b675bd79cb5cd2b&units=imperial";
+const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=c6de52252b8d907f2b675bd79cb5cd2b&units=imperial";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        document.getElementById('current-cond').textContent = jsObject.weather[0].description;
+        document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
+        document.getElementById('high-temp').textContent = Math.round(jsObject.main.temp_max);
+        document.getElementById('wind-speed').textContent = Math.round(jsObject.wind.speed);
+        document.getElementById('humid').textContent = Math.round(jsObject.main.humidity);
+
+        let temp = jsObject.main.temp;
+        let windSpeed = jsObject.wind.speed;
+        let windChill = (35.74 + (0.6 * temp)) - (35.7 * Math.pow(windSpeed, 0.16)) + (0.43 * temp * Math.pow(windSpeed, 0.16));
+        let windChillTemp = Math.round(windChill);
+        document.getElementById('wind-chill').textContent = windChillTemp;
+        
+    });
+
+    fetch(apiURL2)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.table(jsObject);
+
+
+        const list = jsObject['list'];
+        let zero = 0;
+
+        for (let i = 0; zero < 5; i++) { // temporary checking for valid response and data parsing
+            if (list[i].dt_txt.includes("18:00:00")) {
+                zero++;
+
+                let daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+                let date = new Date(list[i].dt_txt);
+                let day = date.getDay();
+                let image = 'https://openweathermap.org/img/w/' + list[i].weather[0].icon + '.png';
+                let desc = list[i].weather[0].description; 
+            
+                document.getElementById(`day` + zero).textContent = daylist[day];
+                document.getElementById(`temp` + zero).textContent = Math.round(list[i].main.temp);
+                document.getElementById(`icon` + zero).setAttribute('src', image);
+                document.getElementById(`icon` + zero).setAttribute('alt', desc);
+              }
+            }
+    
+    });
