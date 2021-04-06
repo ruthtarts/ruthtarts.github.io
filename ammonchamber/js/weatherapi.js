fb743f4703243a3dcfdf842d83bae2ed2ab873bf
ruthtarts <ruthmmecham@byui.edu>
2021-04-06 13:25:11 -0600

    Initial commit

--- ammonchamber/js/weatherapi.js
+++ ammonchamber/js/weatherapi.js
@@ -6,20 +6,29 @@ fetch(apiURL)
     .then((jsObject) => {
         console.log(jsObject);
 
-        document.getElementById('current-cond').textContent = jsObject.current.weather;
+        
+        document.getElementById('current-cond').textContent = jsObject.current.weather[0].description;
         document.getElementById('current-temp').textContent = Math.round(jsObject.current.temp);
         document.getElementById('humid').textContent = Math.round(jsObject.current.humidity);
 
+    });
+
+    fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.table(jsObject);
+
         let zero = 0;
+
         for (let i = 0; zero < 3; i++) { // temporary checking for valid response and data parsing
-
-            let daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-            let date = new Date(jsObject.daily.dt);
-            let day = date.getDay();
-
-            document.getElementById(`day` + zero).textContent = daylist[day];
-            document.getElementById(`temp` + zero).textContent = Math.round(jsObject.daily[i].temp.day);
-        }
-
-
+      
+                let daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+                let date = new Date(jsObject.daily[i].dt);
+                let day = date.getDay();
+                let image = 'https://openweathermap.org/img/w/' + jsObject.daily[i].weather.icon + '.png';
+            
+                document.getElementById(`day` + zero).textContent = daylist[day];
+                document.getElementById(`temp` + zero).textContent = Math.round(jsObject.daily[i].temp.day);
+                document.getElementById(`icon` + zero).setAttribute('src', image);
+              }
     });
